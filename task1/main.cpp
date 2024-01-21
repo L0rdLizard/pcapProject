4b10f1f16f36b05c3ae1b9cd8da8e93cf7b0bc76
LordLizard Notebook <WarriorXKZ@gmail.com>
2024-01-22 00:43:08 +0300

    init commit

---
+++ task1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a;
+
+    cout << "test1\n";
+    cin >> a;
+    
+    cout << "test2\n";
+
+    return 0;
+}
