1af0e0e1465ab44297465627d457362393db029b
LordLizard Notebook <WarriorXKZ@gmail.com>
2024-01-22 02:27:10 +0300

    test points

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
 
+void foo(int &a){
+    a++;
+}
+
+void foo2(int *b){
+    b++;
+}
+
 int main(int argc, char const *argv[])
 {
-    int a;
+    int a = 5;
+    int b = 5;
 
-    cout << "test1\n";
-    cin >> a;
+    foo(a);
+    int *tempB = &b;
+    foo2(tempB);
     
-    cout << "test2\n";
+    cout << a;
+    cout << endl;
+    cout << *tempB << endl;
 
     return 0;
 }
